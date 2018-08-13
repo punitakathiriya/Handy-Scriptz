ba79833a9c6e3176324f57b5a613ca570c723e37
Zanark <kiodeba11@gmail.com>
2018-08-13 22:48:05 +0530

    Captcha generator

---
+++ PHP/Captcha/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+if(isset($_GET['submit']) && !empty($_GET['captcha']) && isset($_SESSION['secure'])){
+    if ($_SESSION['secure'] == $_GET['captcha'])
+        echo 'Succsess!';
+    else
+        echo 'Please refresh and re-enter the captcha';
+}
+
+$_SESSION['secure'] = rand(1000 , 9999);
+
+
+?>
+
+<form action = 'index.php' method = 'get'>
+<image src = 'captcha_generator.php'/>
+Enter the captcah code:
+<input type = 'text' name = 'captcha' required>
+<input type = 'submit' name = 'submit'>
+<form>
